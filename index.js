7cf95bdf38a2c4429644fa00df40f71642cf1950
Tasha <tashingsworth@gmail.com>
2020-09-02 19:29:27 -0400

    added deps and set up boiler plate

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const session = require("express-session")
+const { decodeBase64 } = require("bcryptjs")
+const KnexSessionStore = require("connect-session-knex")(session)
+
+
+const server = express()
+const port = process.env.PORT || 4000
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+server.use(session({
+    resave: false, // avoid recreating sessions that haven't changed
+    saveUninitialized: false, // to comply with GDPR laws
+    secret: "keep it secreat, keep it safe", // cryptographically sign the cookie
+    store: new KnexSessionStore({
+        knex: db, // configured instance of knex
+        createtable: true, // if the sessions tables doesn't exists, create it automatically
+    }),
+}))
+
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        message: "Something went wrong.",
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server listening at http://localhost${port}`)
+})
