64224853666b756b5a30d6de567683ad4fa7b81f
Tasha <tashingsworth@gmail.com>
2020-09-02 20:13:52 -0400

    created migrations and user-model

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const session = require("express-session")
 const { decodeBase64 } = require("bcryptjs")
 const KnexSessionStore = require("connect-session-knex")(session)
 
+const usersRouter = require("../users/users-router.js");
+const authRouter = require('../auth/auth-router.js');
+
 
 const server = express()
 const port = process.env.PORT || 4000
@@ -13,14 +16,17 @@ server.use(helmet())
 server.use(cors())
 server.use(express.json())
 server.use(session({
-    resave: false, // avoid recreating sessions that haven't changed
-    saveUninitialized: false, // to comply with GDPR laws
-    secret: "keep it secreat, keep it safe", // cryptographically sign the cookie
+    resave: false, // avoid recreating sessions that haven't changed.
+    saveUninitialized: false, // to comply with GDPR laws. Prevents saving cookies without users consent.
+    secret: "keep it secreat, keep it safe", // cryptographically sign the cookie.
     store: new KnexSessionStore({
-        knex: db, // configured instance of knex
-        createtable: true, // if the sessions tables doesn't exists, create it automatically
+        knex: db, // configured instance of knex.
+        createtable: true, // if the sessions tables doesn't exists, create it automatically.
     }),
 }))
+
+server.use("/api/users", usersRouter)
+server.use("/api/uth", authRouter)
 
 server.use((err, req, res, next) => {
     console.log(err)
---
+++ users/users-model.js
@@ -0,0 +1,31 @@
+const db = require("../data/config")
+
+function find() {
+    return db("users")
+        .select("id", "username")
+}
+
+function findBy(filter) {
+    return db("users")
+        .where(filter)
+}
+
+function add(user) {
+    const [id] = await db("users")
+        .insert(user, 'id')
+
+        return findById(id)
+}
+
+function findById(id) {
+    return db("users")
+        .where({id})
+        .first()
+}
+
+module.exports = {
+    find,
+    findBy,
+    findById,
+    add,
+}
